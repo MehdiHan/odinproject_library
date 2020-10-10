b3d581b332c3ce698f0f57c9a24da6c8379c2863
MehdiHan <meh.bouchard@outlook.fr>
2020-10-10 15:44:07 +0200

    Removing total number of pages input,adding eventListener and createBook function

--- js/app.js
+++ js/app.js
@@ -1,53 +1,64 @@
 const bookListItems = document.querySelector('.app__booklist__items');
+const titleBook = document.getElementById('title');
+const authorBook = document.getElementById('author');
+const readStatus = document.getElementById('read');
+const notReadStatus = document.getElementById('notread');
+const button = document.querySelector('[type=submit]');
 
 function Book(title, author, status) {
   this.title = title;
   this.author = author;
   this.status = status;
 }
-let library = [
-  {
-    title: `You don't know js`,
-    author: 'Kyle Simpson',
-    status: 'Read',
-  },
-  {
-    title: `You don't know js`,
-    author: 'Kyle Simpson',
-    status: 'Read',
-  },
-];
+let library = [];
+const createCard = (book) => {
+  const bookListItem = document.createElement('li');
+  bookListItem.classList.add('app__booklist__item');
+
+  const bookListCard = document.createElement('div');
+  bookListCard.classList.add('app__booklist__card');
+  bookListItem.appendChild(bookListCard);
+
+  const bookListCardStatus = document.createElement('div');
+  bookListCardStatus.classList.add('app__card__status');
+  bookListCard.appendChild(bookListCardStatus);
+  const bookListCardInfos = document.createElement('div');
+  bookListCardInfos.classList.add('app__card__infos');
+  bookListCard.appendChild(bookListCardInfos);
+
+  const cardStatusImage = document.createElement('img');
+  cardStatusImage.src = '/img/icon_read.png';
+  bookListCardStatus.appendChild(cardStatusImage);
+  const cardStatusText = document.createElement('span');
+  cardStatusText.innerText = book.status;
+  bookListCardStatus.appendChild(cardStatusText);
+  const cardInfosTitle = document.createElement('h2');
+  cardInfosTitle.innerText = book.title;
+  bookListCardInfos.appendChild(cardInfosTitle);
+  const cardInfosAuthor = document.createElement('span');
+  cardInfosAuthor.innerText = book.author;
+  bookListCardInfos.appendChild(cardInfosAuthor);
+
+  bookListItems.appendChild(bookListItem);
+};
 
 const displayBook = (library) => {
   library.forEach((book) => {
-    const bookListItem = document.createElement('li');
-    bookListItem.classList.add('app__booklist__item');
-
-    const bookListCard = document.createElement('div');
-    bookListCard.classList.add('app__booklist__card');
-    bookListItem.appendChild(bookListCard);
-
-    const bookListCardStatus = document.createElement('div');
-    bookListCardStatus.classList.add('app__card__status');
-    bookListCard.appendChild(bookListCardStatus);
-    const bookListCardInfos = document.createElement('div');
-    bookListCardInfos.classList.add('app__card__infos');
-    bookListCard.appendChild(bookListCardInfos);
-
-    const cardStatusImage = document.createElement('img');
-    cardStatusImage.src = '/img/icon_read.png';
-    bookListCardStatus.appendChild(cardStatusImage);
-    const cardStatusText = document.createElement('span');
-    cardStatusText.innerText = book.status;
-    bookListCardStatus.appendChild(cardStatusText);
-    const cardInfosTitle = document.createElement('h2');
-    cardInfosTitle.innerText = book.title;
-    bookListCardInfos.appendChild(cardInfosTitle);
-    const cardInfosAuthor = document.createElement('span');
-    cardInfosAuthor.innerText = book.author;
-    bookListCardInfos.appendChild(cardInfosAuthor);
-
-    bookListItems.appendChild(bookListItem);
+    createCard(book);
   });
 };
+const createBook = (event) => {
+  let title = titleBook.value;
+  let author = authorBook.value;
+  let status = '';
+  if (readStatus.checked === true) {
+    status += 'read';
+  } else {
+    status += 'notread';
+  }
+
+  const newBook = new Book(title, author, status);
+  createCard(newBook);
+};
+button.addEventListener('click', createBook);
 displayBook(library);
